ac3776c7be5886875c1c3f4f6ea43e6c87c66df5
ntwbruce <e0544240@u.nus.edu>
2021-12-25 02:43:00 +0800

    Implement URL redirection based on user ID

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.get("/", async (req, res) => {
   res.render("index", { title: "dumb osu! calculator and shit", date: currentDate });
 });
 
+var __userId;
 var __username;
 var __acc;
 var __rank;
@@ -47,69 +48,76 @@ var originalAcc;
 var originalPP;
 var originalRank;
 
+app.get("/scores", (req, res) => {
+  res.redirect("/");
+}); 
+
 app.post("/scores", async (req, res) => {
   try {
-    var isInit = req.body.init;
-    if (isInit) {
-
-      var subId = req.body.osu_id;
-      var profile = await getProfile(subId);
-
-      __username = profile.username;
-      __acc = profile.userAcc;
-      __rank = profile.userRank;
-      __photo = profile.userPhoto;
-      __banner = profile.userBanner;
-
-      __factor = profile.accFactor;
-      __data = profile.scores;
-      __selected = profile.selected;
-      __total = profile.totalPP;
-      __totalnb = profile.totalPPNoBonus;
-      __bonus = profile.bonusPP;
-
-      originalAcc = __acc;
-      originalPP = __total;
-      originalRank = __rank;
-
-    } else {
-
-      var isInit = req.body.init;
-      var changeID = req.body.changeID;
-      var change = req.body.change;
-      __selected[changeID] = !(change === 'delete');
-      __totalnb = ppCalc(__data, __selected);
-      __total = __totalnb + __bonus;
-      if (__total === originalPP) {
-        __acc = originalAcc;
-        __rank = originalRank;
-      } else {
-        __acc = accCalc(__data, __selected, __factor);
-        __rank = rankCalc(rankingData, __total);
-      }
-
-    }
-
-    res.render("scores", { title: "Delete My Scores",
-      userProfile: { 
-        username: __username, 
-        acc: __acc,
-        rank: __rank,
-        photo: __photo,
-        banner: __banner,
-        data: __data,
-        selected: __selected,
-        total: __total,
-        totalnb: __totalnb,
-        bonus: __bonus,
-        oriacc: originalAcc,
-        oriPP: originalPP,
-        oriRank: originalRank
-      }
-    });
-
+    res.redirect(307, "/scores/" + req.body.osu_id);
   } catch(e) {
     console.log(e);
     res.sendStatus(500);
   }
+});
+
+app.post("/scores/:id(\\d+)", async (req, res) => {
+  var isInit = req.body.init;
+  if (isInit) {
+    __userId = req.body.osu_id;
+    var profile = await getProfile(__userId);
+
+    __username = profile.username;
+    __acc = profile.userAcc;
+    __rank = profile.userRank;
+    __photo = profile.userPhoto;
+    __banner = profile.userBanner;
+
+    __factor = profile.accFactor;
+    __data = profile.scores;
+    __selected = profile.selected;
+    __total = profile.totalPP;
+    __totalnb = profile.totalPPNoBonus;
+    __bonus = profile.bonusPP;
+
+    originalAcc = __acc;
+    originalPP = __total;
+    originalRank = __rank;
+
+  } else {
+
+    var isInit = req.body.init;
+    var changeID = req.body.changeID;
+    var change = req.body.change;
+    __selected[changeID] = !(change === 'delete');
+    __totalnb = ppCalc(__data, __selected);
+    __total = __totalnb + __bonus;
+    if (__total === originalPP) {
+      __acc = originalAcc;
+      __rank = originalRank;
+    } else {
+      __acc = accCalc(__data, __selected, __factor);
+      __rank = rankCalc(rankingData, __total);
+    }
+
+  }
+
+  res.render("scores", { title: "Delete My Scores",
+    userProfile: { 
+      userid: __userId,
+      username: __username, 
+      acc: __acc,
+      rank: __rank,
+      photo: __photo,
+      banner: __banner,
+      data: __data,
+      selected: __selected,
+      total: __total,
+      totalnb: __totalnb,
+      bonus: __bonus,
+      oriacc: originalAcc,
+      oriPP: originalPP,
+      oriRank: originalRank
+    }
+  });
 });
