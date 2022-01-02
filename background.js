c157d0d052e851423e5e87a0a73e167d8f765147
ntwbruce <e0544240@u.nus.edu>
2022-01-03 02:43:19 +0800

    Update background selection
    Every time a user returns to the main page the background will change,
    now by randomly selecting a file from the bg directory so now they don't
    need to be renamed.

--- background.js
+++ background.js
@@ -1,4 +1,10 @@
-export function fetchBackground() {
-  return `/bg/${Math.round(Math.random() * 16)}.jpg`;
-}
+import { readdirSync } from 'fs';
+import path from 'path';
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const files = readdirSync(`${__dirname}/public/bg`);
+
+export function fetchBackground() {
+  const newBg = files[Math.round(Math.random() * files.length)];
+  return `/bg/${newBg}`;
+}
