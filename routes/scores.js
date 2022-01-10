0b37dfe175bc5eb56406c14f66d8589bda7c991a
ntwbruce <e0544240@u.nus.edu>
2022-01-11 01:58:16 +0800

    Add comments to routing methods

--- routes/scores.js
+++ routes/scores.js
@@ -3,6 +3,14 @@ import { addProfile } from "../logic/profile.js";
 import { ppCalc, accCalc, orderDataSets } from '../logic/scores.js';
 import { isOnLeaderboard, rankCalc } from '../logic/ranking.js';
 
+/**
+ * Redirects POST request based on user ID/username input. 
+ * If user submitted a user ID, the osu_id parameter will contain that value.
+ * If user submitted a username, the osu_username parameter will contain that value.
+ * 
+ * @param {*} req is the HTTP request containing user input.
+ * @param {*} res is the HTTP response.
+ */
 export async function redirect(req, res) {
   try {
     if (req.body.osu_id) {
@@ -16,16 +24,25 @@ export async function redirect(req, res) {
   }
 };
 
+/**
+ * Generates scores page.
+ * 
+ * @param {*} req is the HTTP request containing user input forwarded by redirect function.
+ * @param {*} res is the HTTP response.
+ */
 export async function generatePage(req, res) {
 
-  var command = req.body.command;
-  var userIdentifier = req.params.id;
-  var isScoreRender = true;
+  var command = req.body.command; // Initialising page, rearranging or deleting scores
+  var userIdentifier = req.params.id; // Given user ID or username
+  var isScoreRender = true; // Used for when page is to be rendered
 
+  // User scores page is initialised
   if (command === 'init') {
-
+ 
+    // Skip if redirected from username-based URL (i.e. profile already added)
     if (userIdentifier === req.body.osu_id || userIdentifier === req.body.osu_username) {
 
+      // Save profile data from API in database (if exists), returns boolean based on whether profile exists
       var profileAdded;
       if (userIdentifier === req.body.osu_username) {
         profileAdded = await addProfile(userIdentifier, false);
@@ -33,11 +50,14 @@ export async function generatePage(req, res) {
         profileAdded = await addProfile(userIdentifier, true);
       }
 
+      // Redirect to user ID-based URL if userIdentifier is username (i.e. username given by user)
       if (profileAdded.exists) {
         if (userIdentifier === req.body.osu_username) {
           isScoreRender = false;
           res.redirect(307, "/scores/" + profileAdded.userId);
         }
+
+      // Redirect to user not found page if profile does not exist
       } else {
         isScoreRender = false;
         res.redirect("/scores/usernotfound");
@@ -45,13 +65,18 @@ export async function generatePage(req, res) {
 
     }
 
+  // Score rearrangement based on selected statistic (rank, mods etc.)
   } else if (command === 'arrange') {
-    var newArrangement = req.body.arrangement;
     var currentData = getProfile(userIdentifier);
+    var newArrangement = req.body.arrangement;
     var currentArrangement = currentData.arrangement;
+
+    // If the new and current arrangements are the same (same stat selected again), reverse the arrangement
     if (newArrangement === currentArrangement) {
       newArrangement = `${newArrangement}-reverse`;
     } 
+
+    // Rearrange the scores by given new arrangement
     var { newScores, newSelection } = orderDataSets(currentData.scores, currentData.selection, newArrangement);
 
     setProfile(userIdentifier, {
@@ -63,18 +88,21 @@ export async function generatePage(req, res) {
       calculated: currentData.calculated
     });
 
+  // Delete or undelete a score
   } else {
-    var changeID = req.body.changeID;
-    var change = req.body.change;
+    var changeID = req.body.changeID; // ID of score to delete
+    var change = req.body.change; // 'Delete' or 'Undelete'
     var currentData = getProfile(userIdentifier);
 
+    // Obtain updated selection array
     var newSelection = currentData.selection;
     newSelection[changeID] = !(change === 'Delete');
 
+    // Only update main statistics if user is active
     if (!currentData.profile.isInactive) {
 
+      // Obtain updated statistics calculated based on new selection array
       var newPP = ppCalc(currentData.scores, newSelection, currentData.profile.numOfScores) + currentData.precalculated.bonusPP;
-      var newIsOnLeaderboard = isOnLeaderboard(newPP);
       var newAcc;
       var newRank;
       if (currentData.profile.totalPP === newPP) {
@@ -84,6 +112,9 @@ export async function generatePage(req, res) {
         newAcc = accCalc(currentData.scores, newSelection, currentData.precalculated.factor, currentData.profile.numOfScores);
         newRank = newIsOnLeaderboard ? rankCalc(newPP) : currentData.calculated.rank;
       }
+      
+      // Check if rank can be calculated with leaderboards in current ranking data
+      var newIsOnLeaderboard = isOnLeaderboard(newPP);
 
       setProfile(userIdentifier, {
         profile: currentData.profile,
@@ -101,6 +132,7 @@ export async function generatePage(req, res) {
     }
   }
 
+  // Render score page with current user details
   if (isScoreRender) {
     var dataToRender = getProfile(userIdentifier);
     var currentBg = getBg();
@@ -135,6 +167,12 @@ export async function generatePage(req, res) {
   }
 };
 
+/**
+ * Generates user not found page.
+ * 
+ * @param {*} req is the HTTP request.
+ * @param {*} res is the HTTP response.
+ */
 export async function notFound(req, res) {
   var currentBg = getBg();
   res.render("usernotfound", { title: "Delete Your Scores", bg: currentBg });
--- routes/site.js
+++ routes/site.js
@@ -2,14 +2,24 @@ import { fetchRankingData } from '../logic/ranking.js';
 import { fetchBackground } from '../logic/background.js';
 import { getDate, setDate, setBg, getBg } from '../logic/data.js';
 
+/**
+ * Renders main page.
+ * 
+ * @param {*} req is the HTTP request.
+ * @param {*} res is the HTTP response.
+ */
 export async function main(req, res) {
+  // Fetch ranking data at 12am daily
   const today = new Date();
   const currentDate = getDate();
   if (!(currentDate instanceof Date) || currentDate.getFullYear() != today.getFullYear() || currentDate.getMonth() != today.getMonth() || currentDate.getDate() != today.getDate()) {
     await fetchRankingData();
     setDate(today);
   }
+
+  // Get new background on every visit to the main page
   setBg(fetchBackground());
+
   const updatedCurrentBg = getBg();
   const updatedCurrentDate = getDate();
   res.render("index", { title: "Delete Your Scores", date: updatedCurrentDate, bg: updatedCurrentBg });
